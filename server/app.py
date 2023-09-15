f48d0e540d5d220a4f0d36211f0bbcda413815f9
James Hawk <jph94880@gmail.com>
2023-09-15 16:05:31 -0500

    Passes 3 tests

--- server/app.py
+++ server/app.py
@@ -3,6 +3,8 @@
 from flask import Flask, jsonify, request, make_response
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
+from flask_cors import CORS
+import ipdb
 
 from models import db, Plant
 
@@ -15,6 +17,7 @@ migrate = Migrate(app, db)
 db.init_app(app)
 
 api = Api(app)
+CORS(app)
 
 
 class Plants(Resource):
@@ -25,7 +28,6 @@ class Plants(Resource):
 
     def post(self):
         data = request.get_json()
-
         new_plant = Plant(
             name=data['name'],
             image=data['image'],
@@ -36,19 +38,59 @@ class Plants(Resource):
         db.session.commit()
 
         return make_response(new_plant.to_dict(), 201)
+    
+    def patch(self, id):
+        record = Plant.query.filter(Plant.id == id).first()
+        ipdb.set_trace()
+        for attr in request.json:
+            setattr(record, attr, request.json[attr])
+
+        db.session.add(record)
+        db.session.commit()
+
+        response_dict = record.to_dict()
+
+        return make_response(
+            response_dict,
+            200
+        )
+
+    
+    
+        
+# Flask Restful is best suited for returning json with RESTful APIs. @app.route() can return json and html, but is more cumbersome to write and debug than Flask RESTful. Use Flask RESTful if you only intend to return json. 
+
+        
 
 
 api.add_resource(Plants, '/plants')
 
 
-class PlantByID(Resource):
+class PlantById(Resource):
 
     def get(self, id):
-        plant = Plant.query.filter_by(id=id).first().to_dict()
-        return make_response(jsonify(plant), 200)
+        return Plant.query.filter(Plant.id == id).first().to_dict()
+    
+    def patch(self, id):
+        record = Plant.query.filter(Plant.id == id).first()
+        for attr in request.json:
+            setattr(record, attr, request.json[attr])
 
+        db.session.add(record)
+        db.session.commit()
 
-api.add_resource(PlantByID, '/plants/<int:id>')
+        response_dict = record.to_dict()
+
+        return make_response(
+            response_dict,
+            200
+        )
+    
+    def delete(self):
+        return 
+    
+api.add_resource(PlantById, "/plants/<int:id>")
+
 
 
 if __name__ == '__main__':
